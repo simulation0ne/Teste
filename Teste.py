6cbd4f0c5931cae6b747da72ee47fa70f9ca1472
simulation0ne <43868802+simulation0ne@users.noreply.github.com>
2018-10-17 14:10:02 +0000

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -6,6 +6,6 @@ from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="specify_your_app_name_here")
 location = geolocator.geocode("175 5th Avenue NYC")
-print(location.address) Flatiron Building, 175, 5th Avenue, Flatiron, New York, NYC, New York, ...
-print((location.latitude, location.longitude)) (40.7410861, -73.9896297241625)
-print(location.raw){'place_id': '9167009604', 'type': 'attraction', ...}
+print(location.address)
+print((location.latitude, location.longitude))
+print(location.raw)
